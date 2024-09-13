415ddb8c660eb9dec52391f9539e8ef41da39a69
Krnsand <krnsandin@gmail.com>
2024-09-13 13:53:06 +0000

    Lekt med javascript och scss

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "Hello World";
 }
 console.log("hello");
+
+// Hämta elementet
+const box = document.getElementById("box");
+
+// Lägg till en klickhändelse
+box.addEventListener("click", function () {
+  // Lägg till/ta bort klassen 'active' vid varje klick
+  box.classList.toggle("active");
+});
+
+console.log("hejhej");
